61a761ad7b854f05679716efe0b5b0eaf08c1e5e
akash <akash@taufilms.com>
2019-05-28 16:42:40 +0530

    2019-05-28 - edited mayaReference.py

--- dcc/maya/mayaReference.py
+++ dcc/maya/mayaReference.py
@@ -3,19 +3,24 @@ import sys
 import pymel.core as pm
 import maya.cmds as cmds
 
-def get_loaded_refs_list(mode=None):
-    if mode == 'list':
-        return [ref.namespace for ref in pm.listReferences() if ref.isLoaded()]
-    else:
-        return [ref for ref in pm.listReferences() if ref.isLoaded()]
 
-def get_unloaded_refs_list(mode=None):
-    if mode == 'list':
-        return [ref.namespace for ref in pm.listReferences() if not ref.isLoaded()]
-    else:
-        return [ref for ref in pm.listReferences() if not ref.isLoaded()]
+class MayaReference():
+    def __init__(self):
+        pass
 
-def get_nested_refs_list():
-    pass
+    def get_refs_list_loaded(self,mode=None):
+        if mode == 'list':
+            return [ref.namespace for ref in pm.listReferences() if ref.isLoaded()]
+        else:
+            return [ref for ref in pm.listReferences() if ref.isLoaded()]
+
+    def get_refs_list_unloaded(self,mode=None):
+        if mode == 'list':
+            return [ref.namespace for ref in pm.listReferences() if not ref.isLoaded()]
+        else:
+            return [ref for ref in pm.listReferences() if not ref.isLoaded()]
+
+    def get_refs_list_nested(self):
+        pass
 
 
