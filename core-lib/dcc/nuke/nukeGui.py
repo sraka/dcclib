c234f0d8e6c01692e024ca25f5392a8662956b58
akash <akash@taufilms.com>
2019-07-18 16:27:16 +0530

    2019-07-18 - made nukeGui backwork comtable with pyside & pydside2

--- core-lib/dcc/nuke/nukeGui.py
+++ core-lib/dcc/nuke/nukeGui.py
@@ -1,16 +1,20 @@
+
+
 import os
 import sys
-import PySide.QtCore as QtCore
-import PySide.QtGui as QtGui
+import nuke
 
-app = QtGui.QApplication.instance()
+from Qt import QtGui, QtCore , QtWidgets
+
+
+app = QtWidgets.QApplication.instance()
 def NukeWindow():
     for widget in app.topLevelWidgets():
         if widget.metaObject().className() == 'Foundry::UI::DockMainWindow':
             return widget
 
 def register_ui(func,title = None,panel = None):
-    ''' 
+    '''
     Args :      func = Name of Function to be called (str)
                 title = title of the tool (str)
                 panel = Panel name (1 = Properties Panel , 2 = Viewer Panel)
@@ -21,9 +25,9 @@ def register_ui(func,title = None,panel = None):
         panel = 'Properties.1'
     elif panel == 2:
         panel = 'Viewer.1'
-       
+
     if title == None:
         title = 'Test'
-    
+
     pane = nuke.getPaneFor(str(panel))
     panels.registerWidgetAsPanel(str(func), str(title), 'uk.co.thefoundry.{}_UI'.format(title), True).addToPane(pane)
