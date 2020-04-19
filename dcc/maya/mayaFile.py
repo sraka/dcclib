39d5767db50b33cf90f59290f92c3600873849c8
akash <akash@taufilms.com>
2020-04-19 05:57:04 +0530

    2020-04-19 - added few updated in mayaFile

--- dcc/maya/mayaFile.py
+++ dcc/maya/mayaFile.py
@@ -16,3 +16,38 @@ def get_scene_dir():
 def get_workspace_path():
     return str(pm.Workspace().path.dirname())
 
+
+def delete_unknown_nodes():
+    """
+    Delete all the unknown nodes present in the scene file
+    :return:
+    """
+    print "Deleting Unknown Nodes"
+    cmds.delete(cmds.ls(type='unknown'))
+
+
+def delete_unknown_plugins():
+    """
+    Delete all the unknown plugins present in the scene file
+    :return:
+    """
+    plugins_list = cmds.unknownPlugin(q=True, l=True)
+    if plugins_list:
+        for plugin in plugins_list:
+            print(plugin)
+            cmds.unknownPlugin(plugin, r=True)
+
+
+def list_all_plugins(status=all, info=False):
+    """
+    List all plugins , with details
+    :param status:
+    :param info: list the info as well (version,path installed)
+    :return:
+    """
+    if status == "enabled":
+        print "all active plugins"
+    if status == "disabled":
+        print "all inactive plugins"
+    if status == "all":
+        print "List all plugins"
