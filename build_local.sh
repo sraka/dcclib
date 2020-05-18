3cea836f1acb7c8c58e479c16c1662303a03cb9a
akash <akash@taufilms.com>
2020-05-19 02:42:27 +0530

    2020-05-19 - added  feature to create the dir of build for first time build

--- build_local.sh
+++ build_local.sh
@@ -3,25 +3,26 @@
 # this script will only run if the build dir is available and there are at-least one previous build present
 branch='master'
 tag_version=`git describe --tags`
+module_name="corelib"
 
 # import the studio env paths in terminal
 source ~/star/systems/pipe-env.sh
-build_path_dir=${STUDIO_BUILD_DIR}/tools/${branch}/
+build_path_dir=${STUDIO_BUILD_DIR}/${module_name}/${branch}/
+echo ${build_path_dir}
 
 # get the latest build number to be built for
 output_build_no=$(python ./get_build_no.py ${build_path_dir} 2>&1 >/dev/null)
 echo ${output}
 
 # construct the build dir
-build_path=${STUDIO_BUILD_DIR}/tools/${branch}/${tag_version}-${output_build_no}
+build_path=${STUDIO_BUILD_DIR}/${module_name}/${branch}/${tag_version}-${output_build_no}
 
 # sync to file to its latest
 #git checkout origin ${branch}
 script_dir=`dirname $0 | xargs realpath`
 rsync -avz --exclude '.*' ${script_dir}/ ${build_path}
-echo ${build_path}
 echo ${script_dir}
-
+echo ${build_path}
 
 
 
--- get_build_no.py
+++ get_build_no.py
@@ -6,10 +6,15 @@ import sys
 import glob
 build_dir = sys.argv[1]
 
-# full_path = /mnt/e/programming/code/star/dev/builds/tools/master/v0.0.3-4/
-full_path = max(glob.glob(os.path.join(build_dir, '*/')), key=os.path.getmtime)
-build_no = full_path.split("/")[-2].split("-")[1]
-new_build_no = (int(build_no) + 1)
+# For a fresh module , if the build dir is not there , then create it
+if not os.path.exists(build_dir):
+    os.makedirs(build_dir)
+    sys.exit(str(1))
+else:
+    # full_path = /mnt/e/programming/code/star/dev/builds/tools/master/v0.0.3-4/
+    full_path = max(glob.glob(os.path.join(build_dir, '*/')), key=os.path.getmtime)
+    build_no = full_path.split("/")[-2].split("-")[1]
+    new_build_no = (int(build_no) + 1)
 
-# return
-sys.exit(str(new_build_no))
+    # return
+    sys.exit(str(new_build_no))
