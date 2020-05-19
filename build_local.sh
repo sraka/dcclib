50881ed440b5785472ae3a2e667cc209d9f145a8
akash <akash@taufilms.com>
2020-05-20 02:15:31 +0530

    2020-05-20 - updated build scripts

--- build_local.sh
+++ build_local.sh
@@ -2,27 +2,29 @@
 
 # this script will only run if the build dir is available and there are at-least one previous build present
 branch='master'
+script_dir=`dirname $0 | xargs realpath`
 tag_version=`git describe --tags`
-module_name="corelib"
+module_name=`basename ${script_dir}`
+echo ${script_dir} , ${module_name}
 
 # import the studio env paths in terminal
 source ~/star/systems/pipe-env.sh
 build_path_dir=${STUDIO_BUILD_DIR}/${module_name}/${branch}/
-echo ${build_path_dir}
+echo "Build dir = ${build_path_dir}"
 
 # get the latest build number to be built for
 output_build_no=$(python ./get_build_no.py ${build_path_dir} 2>&1 >/dev/null)
-echo ${output}
 
 # construct the build dir
 build_path=${STUDIO_BUILD_DIR}/${module_name}/${branch}/${tag_version}-${output_build_no}
+echo ${build_path}
 
 # sync to file to its latest
 #git checkout origin ${branch}
-script_dir=`dirname $0 | xargs realpath`
 rsync -avz --exclude '.*' ${script_dir}/ ${build_path}
-echo ${script_dir}
 echo ${build_path}
+
+
 
 
 
--- get_build_no.py
+++ get_build_no.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""
+This file :
+returns the latest build number to sh file
+updates the __version__ file with latest release no
+"""
 
 import os
 import sys
 import glob
-build_dir = sys.argv[1]
+import subprocess
 
-# For a fresh module , if the build dir is not there , then create it
+
+build_dir = sys.argv[1]   # /mnt/e/programming/code/star/dev/builds/tools/master/
+print(build_dir)
+
+def update_version_file(new_version):
+    """
+    # Write the latest version in the __version__ file
+    :return:
+    """
+    with open("__version__", 'w') as f:
+        f.write("__version__=\"{}\"".format(str(new_version)))
+
 if not os.path.exists(build_dir):
+    # For a fresh module , if the build dir is not there , then create it
     os.makedirs(build_dir)
     sys.exit(str(1))
 else:
-    # full_path = /mnt/e/programming/code/star/dev/builds/tools/master/v0.0.3-4/
+    print("------")
+    # get the latest folder created by date
+    # full_path = /mnt/e/programming/code/star/dev/builds/tools/master/0.0.3-4/
     full_path = max(glob.glob(os.path.join(build_dir, '*/')), key=os.path.getmtime)
-    build_no = full_path.split("/")[-2].split("-")[1]
-    new_build_no = (int(build_no) + 1)
+    cur_build_no = full_path.split("/")[-2].split("-")[1]
+    print(full_path, "current build no : {}".format(cur_build_no))
+    new_build_no = (int(cur_build_no) + 1)
+    base_name = os.path.basename(os.path.dirname(full_path))      # base_name = 0.0.7-17
+
+    # Get the tag version
+    tag_version = subprocess.check_output("git describe --tags", stderr=subprocess.STDOUT, shell=True)
+    tag_version = tag_version.strip()
+
+    # Build the new version name and update in __version__ file
+    new_version = "{}-{}".format(tag_version, new_build_no)
+    print(base_name, new_version)
+    update_version_file(new_version)
 
     # return
     sys.exit(str(new_build_no))
