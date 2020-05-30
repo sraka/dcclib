9fbf6b06b150be6180fca41a5be454107ff2dc18
akash <artistakash.3d@gmail.com>
2020-05-30 18:14:43 +0530

    2020-05-30 - updated the build.sh file to use updated PIPELNE_BUILD_DIR env variable

--- build_local.sh
+++ build_local.sh
@@ -8,15 +8,15 @@ module_name=`basename ${script_dir}`
 echo ${script_dir} , ${module_name}
 
 # import the studio env paths in terminal
-source ~/star/systems/pipe-env.sh
-build_path_dir=${STUDIO_BUILD_DIR}/${module_name}/${branch}/
+source ~/studio/systems/pipe-env.sh
+build_path_dir=${PIPELINE_BUILD_DIR}/${module_name}/${branch}/
 echo "Build dir = ${build_path_dir}"
 
 # get the latest build number to be built for
 output_build_no=$(python ./get_build_no.py ${build_path_dir} 2>&1 >/dev/null)
 
 # construct the build dir
-build_path=${STUDIO_BUILD_DIR}/${module_name}/${branch}/${tag_version}-${output_build_no}
+build_path=${PIPELINE_BUILD_DIR}/${module_name}/${branch}/${tag_version}-${output_build_no}
 echo ${build_path}
 
 # sync to file to its latest
